99c0603ba7a3414b982477dd4fa450d96863469b
GanWenHaoIsaac <isaacgan64@gmail.com>
2020-06-18 15:24:05 +0800

    Implemented the features into Unity project

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    public GameObject Obstacle;
 
     void Start()
     {
@@ -15,6 +20,29 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector3.up * 300);
+        }
+
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (screenPosition.y < -4)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            SceneManager.LoadScene("GameOver");
+            
+        }
+    }
+
 }
